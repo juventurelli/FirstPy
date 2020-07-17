b5aa6e3264cb8c0a1ed85f90f41a81f55cf407f1
juliane.venturelli <juliane.venturelli@gmail.com>
2020-07-16 22:48:02 -0300

    maquina

--- MaquinaAgua.py
+++ MaquinaAgua.py
@@ -1,3 +1,19 @@
 class MaquinaAgua:
-    def __init__(self,quant_agua):
-        self.quant_agua = quant_agua
+    quant_maxima = 20000
+    
+    def __init__(self,quant_agua_left = None,quant_200_left= None):
+        self.quant_agua_left = quant_agua_left
+        self.quant_200_left = quant_200_left
+
+    def say_quant_200_left(self):
+        print(self.quant_200_left)
+
+    def set_quant200(self, quant_200_left):
+        self.quant_200_left = quant_200_left
+        
+    def get_quant200(self):
+        return self.quant_200_left 
+
+x = MaquinaAgua()
+x.set_quant200(100)
+print(x.say_quant_200_left())
---
+++ mainMaquinaAgua.py
@@ -0,0 +1,5 @@
+import MaquinaAgua
+
+x = MaquinaAgua()
+x.set_quant200(10)
+print(x.get_quant200)
