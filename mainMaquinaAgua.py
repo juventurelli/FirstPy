7dc0e57201bb6f2a3a0205f20733af18f22f8c19
juliane.venturelli <juliane.venturelli@gmail.com>
2020-07-17 20:38:27 -0300

    agua pronta

--- mainMaquinaAgua.py
+++ mainMaquinaAgua.py
@@ -3,5 +3,31 @@ import MaquinaAgua as MA
 maq = MA.MaquinaAgua()
 
 print(maq.quant_agua)
+print(maq.quant_200)
+print(maq.quant_300)  
+
+maq.servirCopo200(maq.quant_200,maq.quant_agua)
+maq.servirCopo300(maq.quant_300,maq.quant_agua)
+
 maq.abastecerAgua()
-#print
+maq.abastecerCopo300()
+
+maq.servirCopo200(maq.quant_200,maq.quant_agua)
+maq.abastecerCopo200()
+maq.abastecerCopo200()
+maq.abastecerCopo200()
+
+for i in range(101): 
+    maq.servirCopo300(maq.quant_300,maq.quant_agua)
+
+#maq.servirCopo300(maq.quant_300,maq.quant_agua)
+#maq.servirCopo300(maq.quant_300,maq.quant_agua)
+#maq.servirCopo300(maq.quant_300,maq.quant_agua)
+#maq.servirCopo200(maq.quant_200,maq.quant_agua)
+#maq.servirCopo200(maq.quant_200,maq.quant_agua)
+
+#maq.servirCopo200(maq.quant_200,maq.quant_agua)
+#maq.servirCopo200(maq.quant_200,maq.quant_agua)
+
+#maq.abastecerCopo300()
+#maq.servirCopo200(maq.quant_200,maq.quant_agua)
