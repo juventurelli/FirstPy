13503678053f747f700051f751c536a80ba79cb0
juliane.venturelli <juliane.venturelli@gmail.com>
2020-07-18 21:32:37 -0300

    get_set

--- mainMaquinaAgua.py
+++ mainMaquinaAgua.py
@@ -1,33 +1,135 @@
-import MaquinaAgua as MA
+from WaterDispenser import WaterDispenser
 
-maq = MA.MaquinaAgua()
+def main():
 
-print(maq.quant_agua)
-print(maq.quant_200)
-print(maq.quant_300)  
+  maq = WaterDispenser()
 
-maq.servirCopo200(maq.quant_200,maq.quant_agua)
-maq.servirCopo300(maq.quant_300,maq.quant_agua)
+  print(maq.agua()==0) # mL
+  print(maq.copos_200() == 0)
+  print(maq.copos_300() == 0)
+  maq.servir_copo_200(maq.quant_copos_200,maq.quant_agua) # não efetua
 
-maq.abastecerAgua()
-maq.abastecerCopo300()
+  print(maq.agua() == 0) # mL
+  print(maq.copos_200() == 0)
+  print(maq.copos_300() == 0)
 
-maq.servirCopo200(maq.quant_200,maq.quant_agua)
-maq.abastecerCopo200()
-maq.abastecerCopo200()
-maq.abastecerCopo200()
+  maq.abastecerAgua() # inicializa 20000mL
+  print(maq.agua() == 20000) # mL
 
-for i in range(101): 
-    maq.servirCopo300(maq.quant_300,maq.quant_agua)
+  maq.abastecerAgua() # mantém consistente
+  print(maq.agua() == 20000) # mL
 
-#maq.servirCopo300(maq.quant_300,maq.quant_agua)
-#maq.servirCopo300(maq.quant_300,maq.quant_agua)
-#maq.servirCopo300(maq.quant_300,maq.quant_agua)
-#maq.servirCopo200(maq.quant_200,maq.quant_agua)
-#maq.servirCopo200(maq.quant_200,maq.quant_agua)
+  maq.servir_copo_200(maq.quant_copos_200,maq.quant_agua) # não efetua, sem copo
+  print(maq.agua() == 20000) # mL
 
-#maq.servirCopo200(maq.quant_200,maq.quant_agua)
-#maq.servirCopo200(maq.quant_200,maq.quant_agua)
+  maq.servir_copo_200(maq.quant_copos_200,maq.quant_agua) # agora a máquina possui 100 copos de 200
+  print(maq.copos_200() == 100)
 
-#maq.abastecerCopo300()
-#maq.servirCopo200(maq.quant_200,maq.quant_agua)
+#   # correção (A.K.A. PATCH!) =====================================
+#   maq.servir_copo_200()
+#   maq.servir_copo_200()
+#   maq.servir_copo_200()
+#   maq.servir_copo_200()
+#   maq.servir_copo_200() # -1000ml e 5 copos de 200
+
+#   print(maq.agua() == 19000)
+#   print(maq.copos_200() == 95)
+#   print(maq.copos_300() == 0)
+
+#   maq.abastecer_copo_200() # agora a máquina possui 100 copos de 200 novamente
+#   print(maq.copos_200() == 100)
+
+#   maq.servir_copo_200() # -200ml e 1 copo de 200
+#   # fim correção =====================================
+
+#   print(maq.agua() == 18800)
+#   print(maq.copos_200() == 99)
+#   print(maq.copos_300() == 0)
+
+#   maq.abastecer_copo_300() # agora a máquina possui 100 copos de 300
+#   print(maq.copos_300() == 100)
+
+#   maq.servir_copo_300()
+#   print(maq.agua() == 18500)
+#   print(maq.copos_200() == 99)
+#   print(maq.copos_300() == 99)
+
+#   # servir 50 copos de 300 = 15000ml
+#   for i in range (0, 50, 1):
+#     maq.servir_copo_300()
+
+#   print(maq.agua() == 3500)
+#   print(maq.copos_200() == 99)
+#   print(maq.copos_300() == 49)
+
+#   # servir 17 copos de 200 = 3400ml
+#   for i in range (0, 17, 1):
+#     maq.servir_copo_200()
+
+#   print(maq.agua() == 100)
+#   print(maq.copos_200() == 82)
+#   print(maq.copos_300() == 49)
+
+#   # não há água para atender o pedido
+#   maq.servir_copo_300()
+
+#   print(maq.agua() == 100)
+#   print(maq.copos_200() == 82)
+#   print(maq.copos_300() == 49)
+
+#   # não há água para atender o pedido
+#   maq.servir_copo_200()
+
+#   print(maq.agua() == 100)
+#   print(maq.copos_200() == 82)
+#   print(maq.copos_300() == 49)
+
+#   maq.abastecerAgua() # inicializa 20000mL
+
+#   print(maq.agua() == 20000)
+#   print(maq.copos_200() == 82)
+#   print(maq.copos_300() == 49)
+
+#   # servir os 49 copos de 300 restantes = 14700ml
+#   while (maq.copos_300() > 0):
+#     maq.servir_copo_300()
+
+#   print(maq.agua() == 5300)
+#   print(maq.copos_200() == 82)
+#   print(maq.copos_300() == 0)
+
+#   # não há copo para atender o pedido
+#   maq.servir_copo_300()
+
+#   print(maq.agua() == 5300)
+#   print(maq.copos_200() == 82)
+#   print(maq.copos_300() == 0)
+
+#   maq.servir_copo_200() # de 200 ok
+#   maq.servir_copo_200() # de 200 ok
+
+#   print(maq.agua() == 4900)
+#   print(maq.copos_200() == 80)
+#   print(maq.copos_300() == 0)
+
+#   maq.abastecer_copo_300() # 100 copos de 300
+
+#   print(maq.agua() == 4900)
+#   print(maq.copos_200() == 80)
+#   print(maq.copos_300() == 100)
+
+#   # 3 copos de 300
+#   maq.servir_copo_300() 
+#   maq.servir_copo_300() 
+#   maq.servir_copo_300()
+
+#   print(maq.agua() == 4000)
+#   print(maq.copos_200() == 80)
+#   print(maq.copos_300() == 97)
+
+if __name__ == "__main__":
+    print('Terminal execution!')
+    main()
+else:
+  print('Module execution!')
+  main()
